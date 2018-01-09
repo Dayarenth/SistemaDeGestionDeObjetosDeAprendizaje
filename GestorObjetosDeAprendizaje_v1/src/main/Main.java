2d7fc8edea1c351db89889a4d03b533a889de82e
Dayarenth <35228880+Dayarenth@users.noreply.github.com>
2018-01-09 10:54:00 -0500

    Version 01

---
+++ GestorObjetosDeAprendizaje_v1/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import utils.JFrameWindow;
+
+public class Main{
+    public static void main(String[] args) {
+    	JFrameWindow window = new JFrameWindow();
+        window.setVisible(true);
+    }
+}
+ 
+    
