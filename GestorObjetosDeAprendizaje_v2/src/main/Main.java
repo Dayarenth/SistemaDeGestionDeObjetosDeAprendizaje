a07c80e9594744536fa86965877c8562c5ecd9b7
Edison <eddyferch@hotmail.es>
2018-01-30 18:06:28 -0500

    Act2

--- GestorObjetosDeAprendizaje_v2/src/main/Main.java
+++ GestorObjetosDeAprendizaje_v2/src/main/Main.java
@@ -9,13 +9,4 @@ public class Main{
     }
 }
  
-package main;
-
-import db.operacionesSobreDB;
-import utils.GuiMainWindow;
-
-public class Main{
-    public static void main(String[] args) {
-    	
-    }
-}    
+    
