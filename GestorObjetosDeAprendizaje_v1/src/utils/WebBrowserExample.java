6cae709e822d2b36b2ef26739a8472d2d0b92a20
Elsa Vasco <maguoandy@hotmail.com>
2018-03-05 21:01:25 -0500

    eliminacion

--- GestorObjetosDeAprendizaje_v1/src/utils/WebBrowserExample.java
+++
@@ -1,8 +0,0 @@
-package utils;
-public class WebBrowserExample {
-    public static void main(String[] args) {
-        JFrameWindow window = new JFrameWindow();
-        window.cargarHTMLFile("file:///C:/wamp/www/shadowplay_1/index.html");
-        window.setVisible(true);
-    }
-}
